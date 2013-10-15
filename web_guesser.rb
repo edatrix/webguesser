d4d7103515e2a9e7249863cec544e5a098ab370d
edatrix <lespath@gmail.com>
2013-10-15 09:18:31 -0600

    initial server structure run-through with Jeff)(

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,6 +3,18 @@ require 'sinatra/reloader'
 
 number = rand(101)
 
+def check_guess(guess)
+  if guess == number
+    message = "You got it!"
+  elsif guess > number + 5
+    message = " you are way too high"
+  else guess < number - 5
+    message = "tooooooo loooooow"
+  end
+end
+
 get'/' do
   erb :index, :locals => {:number => number}
+  params["guess"].to_i
+  message = check_guess(guess)
 end
