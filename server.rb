e3c1c6e290e51e824c83602236185b5179a6f507
edatrix <lespath@gmail.com>
2013-10-15 10:50:40 -0600

    I3, number guessing game complete, without extensions

--- server.rb
+++ server.rb
@@ -1,7 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+set :secret, rand(100)
+# number = rand(100)
+
 get '/' do
-  secret_number = rand(100)
-  erb :index, :locals => {:secret_number => secret_number}
-end
+  guess = params[:guess].to_i
+  message = feedback(settings.secret, guess)
+  # if guess == number
+  #   message = "correct!"
+  # else
+  #   message = "not even close!"
+  # end
+  erb :index, :locals => {:secret => settings.secret,
+                          :guess => guess,
+                          :message => message}
+  end
+
+  def feedback(number, guess)
+    if guess == number
+      "Correct!"
+    elsif guess > number + 5
+      "way too high"
+    elsif guess > number
+      "too high"
+    elsif guess < number - 5
+      "way too low"
+    else guess < number
+      "your guess is too low"
+    end
+  end
